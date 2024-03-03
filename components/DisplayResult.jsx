fd36b608e989be1124adfd3a0430001e0cd68e99
Aftiyan <aftiyangamer45@gmail.com>
2024-03-03 20:04:59 +0700

    tambah loading state fix accuracy

--- components/DisplayResult.jsx
+++ components/DisplayResult.jsx
@@ -1,13 +1,25 @@
 "use client";
 
-import React from "react";
+import React, { useEffect, useState } from "react";
 import { Button } from "@/components/ui/button";
 import { dataset } from "@/data/dataset";
+import { ReloadIcon } from "@radix-ui/react-icons";
 
 const DisplayResult = () => {
-  const handleClick = () => {
-    let success = 0;
-    let error = 0;
+  const [isLoading, setIsLoading] = useState(false);
+  const [success, setSuccess] = useState(0);
+  const [error, setError] = useState(0);
+  const [accuracy, setAccuracy] = useState(0);
+  const [showResult, setShowResult] = useState(false);
+
+  const handleClick = async () => {
+    setIsLoading(true);
+    setShowResult(false);
+    setSuccess(0);
+    setError(0);
+    setAccuracy(0);
+
+    await new Promise(resolve => setTimeout(resolve, 2500))
 
     dataset.map((item) => {
       let result = runPrediction(
@@ -18,16 +30,22 @@ const DisplayResult = () => {
       );
 
       if (result === item.weather) {
-        success++;
+        setSuccess((prev) => prev + 1);
       } else {
-        error++;
-        return `Hasil prediksi ${result}, seharusnya ${item.weather}`;
+        setError((prev) => prev + 1);
       }
     });
 
-    console.log(`Success: ${success}`);
-    console.log(`Error: ${error}`);
-    console.log(`Accuracy: ${success/(success + error) * 100}%`);
+    setShowResult(true);
+    setIsLoading(false);
+  };
+
+  useEffect(() => {
+    setAccuracy((success / (success + error)) * 100);
+  });
+
+  const handleResetClick = () => {
+    setShowResult(false);
   };
 
   const runPrediction = (precipitation, temp_max, temp_min, wind) => {
@@ -39,19 +57,54 @@ const DisplayResult = () => {
           }
         }
       }
+      return "drizzle";
+    } else {
+      if (temp_max <= 20.3 && temp_min >= 0.6) {
+        return "rain";
+      } else if (temp_max <= 7.2) {
+        if (temp_max >= -1.1 && temp_min <= 0.6) {
+          return "snow";
+        }
+      }
     }
   };
 
   return (
     <div className="flex flex-col w-full gap-y-3">
-      <Button
-        onClick={handleClick}
-        className="px-7 dark:bg-pink-400 dark:hover:bg-pink-500 self-start"
-      >
-        Run
-      </Button>
+      <div className="flex gap-x-3">
+        <Button
+          onClick={handleClick}
+          className={`px-7 self-start ${isLoading ? '' : 'dark:bg-pink-400 dark:hover:bg-pink-500'}`}
+          disabled={isLoading ? true : false}
+        >
+          {isLoading && <ReloadIcon className="mr-2 h-4 w-4 animate-spin" />}
+          {isLoading ? "Tunggu" : "Run"}
+        </Button>
+        <Button
+          onClick={handleResetClick}
+          className="px-7 dark:bg-pink-400 dark:hover:bg-pink-500 self-start"
+        >
+          Reset
+        </Button>
+      </div>
       <div className="border border-gray-700 rounded-md p-3 min-h-72">
-        malas
+        {showResult && (
+          <>
+            <p>
+              Success:{" "}
+              <span className="font-semibold text-green-400">
+                {success} predicted correctly
+              </span>
+            </p>
+            <p>
+              Error:{" "}
+              <span className="font-semibold text-red-500">{error} wrong</span>
+            </p>
+            <p>
+              Accuracy: <span className="font-semibold">{accuracy}%</span>
+            </p>
+          </>
+        )}
       </div>
     </div>
   );
